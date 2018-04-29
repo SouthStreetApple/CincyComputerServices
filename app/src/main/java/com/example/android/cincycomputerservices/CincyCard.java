0b4892a7751a99b54ebad149467b469ecdae89a9
Jameson <jamesonwking@gmail.com>
2018-04-28 21:18:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/cincycomputerservices/CincyCard.java
@@ -0,0 +1,13 @@
+package com.example.android.cincycomputerservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CincyCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cincy_card);
+    }
+}
